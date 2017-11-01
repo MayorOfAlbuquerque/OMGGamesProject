83c147496e0987605f397ae1e25b1716d6510e16
Rachel Jeffries-Harris <Rae@eduroam-25-117.nomadic.bris.ac.uk>
2017-11-01 16:13:53 +0000

    Initial movement achieved

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		transform.position += new Vector3 (h * 0.1f, 0f, v * 0.1f);
+	}
+}
