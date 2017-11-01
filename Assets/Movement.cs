8739e4ebfbeae6da253bafa26490a31f5aa9a878
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2017-11-01 18:02:56 +0000

    Object now moves in direction of camera, but there is some weird loop back to origin stuff happening

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -14,6 +14,18 @@ public class Movement : MonoBehaviour {
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 
-		transform.position += new Vector3 (h * 0.1f, 0f, v * 0.1f);
-	}
+        Vector3 targetDirection = new Vector3(h, transform.position.y, v);
+
+        //Vector3 targetDirection = transform.position;
+
+        targetDirection = Camera.main.transform.TransformDirection(targetDirection);
+        targetDirection.x += (h* 0.1f);
+        targetDirection.z += (v* 0.1f);
+
+        
+
+        //targetDirection += Camera.main.transform.TransformDirection(h * 0.1f, 0f, v * 0.1f);
+
+        transform.position = targetDirection;
+    }
 }
