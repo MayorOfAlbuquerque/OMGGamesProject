45a35e2a34b20dd5c44ce5915c9143858e361930
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2017-11-03 10:51:12 +0000

    Forgot to do add all on git

---
+++ Assets/Assets/_src/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+        Vector3 newPos;
+        
+        if (h != 0)
+        {
+            newPos = transform.position + Camera.main.transform.right * h;
+            newPos.y = 1f; // this is to avoid flying
+        }
+        else if (v != 0)
+        {
+            newPos = transform.position + Camera.main.transform.forward * v;
+            newPos.y = 1f; // This is to avoid flying
+        }
+        else newPos = transform.position;
+ 
+        transform.position = newPos;
+   
+    }
+}
