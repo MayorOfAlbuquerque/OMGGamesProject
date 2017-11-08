23e69c3b54bf6d62f5f43ec0ffb88552dfdbd790
Rachel Jeffries-Harris <Rae@eduroam-25-117.nomadic.bris.ac.uk>
2017-11-08 14:37:37 +0000

    Going to push to feature branch

---
+++ Assets/Assets/_src/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public float Speed;
+	public Movement Movement;
+
+	void Start () {
+		Movement = new Movement (Speed);
+	}
+
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+        Vector3 newPos;
+        
+        if (h != 0 || v!= 0)
+        {
+			Vector3 right = Camera.main.transform.right;
+			Vector3 forward = Camera.main.transform.forward;
+			newPos = Movement.Calculate (transform.position,h,v,right,forward);
+        }
+        else newPos = transform.position;
+ 
+        transform.position = newPos;
+    }
+		
+}
