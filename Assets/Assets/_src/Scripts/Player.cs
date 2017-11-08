ff988148cc5a9677724272bd4c086f96b9fefe47
Sydney <sd14814@my.bristol.ac.uk>
2017-11-08 18:04:33 +0000

    Basic Networking
    Basic networking with the VR cube

---
+++ Assets/Assets/_src/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+
+public class Player : NetworkBehaviour {
+
+    [System.Serializable]
+    public class ToggleEvent : UnityEvent<bool> { }
+
+        [SerializeField] ToggleEvent onToggleShared;
+        [SerializeField] ToggleEvent onToggleRemote;
+        [SerializeField] ToggleEvent onToggleLocal;
+        GameObject mainCamera;
+
+
+        private void Start()
+        {
+            mainCamera = Camera.main.gameObject;
+            EnablePlayer();
+
+        }
+
+        private void EnablePlayer()
+        {
+            onToggleShared.Invoke(true);
+            if (isLocalPlayer)
+            {
+                mainCamera.SetActive(false);
+                onToggleLocal.Invoke(true);
+            }
+            else
+            {
+                onToggleRemote.Invoke(true);
+            }
+        }
+
+        private void DisablePlayer()
+        {
+            onToggleShared.Invoke(false);
+            if (isLocalPlayer)
+            {
+                mainCamera.SetActive(true);
+                onToggleLocal.Invoke(false);
+            }
+            else
+            {
+                onToggleRemote.Invoke(false);
+            }
+        }
+
+    }
