7a619de1232d1947566de40770dcd0f2061e2354
Unknown <nd14127@my.bristol.ac.uk>
2017-11-10 16:27:38 +0000

    Added introScene to connect to game, can't get tirgger not registering

---
+++ Assets/Assets/_src/Scripts/LoadLevelTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelTrigger : MonoBehaviour {
+
+    [SerializeField] string levelToLoad;
+
+    private void OnTriggerEnter(Collider other) {
+
+        Debug.Log("adf");
+        if(other.gameObject.tag == "Player") {
+            SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
+        }
+    }
+}
