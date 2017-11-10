f96eee75b1ea4351fb00e6203114aab17966e95c
Nathan <nd14127@my.bristol.ac.uk>
2017-11-10 16:49:36 +0000

    commented only

--- Assets/Assets/_src/Scripts/LoadLevelTrigger.cs
+++ Assets/Assets/_src/Scripts/LoadLevelTrigger.cs
@@ -7,9 +7,11 @@ public class LoadLevelTrigger : MonoBehaviour {
 
     [SerializeField] string levelToLoad;
 
+    //triggers when another object enters its area.
     private void OnTriggerEnter(Collider other) {
 
         Debug.Log("adf");
+        //if object is player then load scene
         if(other.gameObject.tag == "Player") {
             SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
         }
