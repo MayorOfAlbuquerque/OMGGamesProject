41ce5691f3cc278741b80785f6015cc48be152b3
Sydney <sd14814@my.bristol.ac.uk>
2017-11-14 00:56:22 +0000

    Auto Connect
    Offline and online scene. Can connect as server, client or host through movement.

---
+++ Assets/Assets/_src/Scripts/JoinAsClient.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class JoinAsClient : MonoBehaviour
+{
+    //triggers when another object enters its area.
+    private void OnTriggerEnter(Collider other)
+    {
+        //if object is player then load scene
+        if (other.gameObject.tag == "Player")
+        {
+            if(NetworkManager.singleton.)
+            Debug.Log("Joined as client");
+            NetworkManager.singleton.StartClient(); //Join as client, only changes to online scene if
+        }
+    }
+}
