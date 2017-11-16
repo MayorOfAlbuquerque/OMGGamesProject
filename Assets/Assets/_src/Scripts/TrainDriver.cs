160ead9b1d6b9b8e8185036744777bed7acaf505
Zaiyang Li <zaiyangli777@gmail.com>
2017-11-16 00:19:57 +0000

    Testing setup where a player is on a moving train, so far it is working.

---
+++ Assets/Assets/_src/Scripts/TrainDriver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainDriver : MonoBehaviour {
+    public Rigidbody trainEngine;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        if (speed < 0.1f) {
+            speed = 1.0f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (trainEngine != null)
+        {
+            trainEngine.velocity = Vector3.forward  * -speed;
+        }
+	}
+}
