f466948bb985ec88561a87322f2dd0f228ca1610
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2017-11-21 15:19:59 +0000

    Added VoiceChat Library. Creating Test Scene

---
+++ Assets/Assets/_src/Scripts/PlayerVoiceChat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVoiceChat : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        
+        Application.RequestUserAuthorization(UserAuthorization.Microphone);
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
