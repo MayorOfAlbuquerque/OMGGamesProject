219a64b2f9e3ca8d3455df846fe38ea478d60e45
Nathan <nd14127@my.bristol.ac.uk>
2017-11-23 16:17:00 +0000

    Pickup weapon basics

---
+++ Assets/Assets/_src/Scripts/PickupController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+public class PickupController : InteractableObjectController , IGvrPointerHoverHandler{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnKeyDown(KeyCode code) { }
+    public override void OnKeyUp(KeyCode code) { }
+
+    public override void OnClick()
+    {
+    }
+
+    public override void OnClick(object obj)
+    {
+        Debug.Log("Player Clicked Object");
+        Player player = obj as Player;
+        player.transform.GetChild(0).gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("Hover over object");
+    }
+}
