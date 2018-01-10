de595f4160a46959f723d3dd5a722a8bf5ea75f5
Nathan <nd14127@my.bristol.ac.uk>
2018-01-10 16:35:38 +0000

    Text appears over box and always faces player. Badly written code

---
+++ Assets/Assets/_src/Scripts/TextOnHover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class TextOnHover : MonoBehaviour, IGvrPointerHoverHandler
+{
+    [SerializeField] GameObject text;                                                       //This is all going to be shit code
+    private int countdown;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //turn off text
+        if(text.activeInHierarchy && countdown <= 0)
+        {
+            text.SetActive(false);
+        }
+        //turn on/leave text on
+        else if(countdown > 0 )
+        {
+            countdown--;
+            if(!text.activeInHierarchy)
+            {
+                text.SetActive(true);
+            }
+        }
+        text.transform.LookAt(Camera.main.transform);
+    }
+
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("Hovering to reveal text");
+
+        countdown = 50;
+    }
+
+}                                                               // Yep it's a horrible bodge
