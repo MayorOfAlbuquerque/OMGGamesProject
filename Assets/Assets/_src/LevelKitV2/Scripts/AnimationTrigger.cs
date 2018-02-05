a947a949e1bf872850ce2350186a2f2e8290ec19
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-05 11:30:29 +0000

    Adding animation to doors

---
+++ Assets/Assets/_src/LevelKitV2/Scripts/AnimationTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class AnimationTrigger : MonoBehaviour {
+    public PlayableDirector director;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("playing animation");
+        Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "Player") {
+            PlayAnimation();
+        }
+    }
+
+    public void PlayAnimation() {
+        if(director == null) {
+            Debug.LogWarning("playable director is null");
+            return;
+        }
+        director.Play();
+    }
+}
