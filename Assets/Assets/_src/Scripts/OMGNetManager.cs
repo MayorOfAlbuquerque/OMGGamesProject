f43115dfcf8e9fefe126598bad367d556b18c835
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-05 21:50:30 +0000

    fixing runtime errors by removing voice chat from player prefab

--- Assets/Assets/_src/Scripts/OMGNetManager.cs
+++ Assets/Assets/_src/Scripts/OMGNetManager.cs
@@ -1,59 +1,11 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.Networking.NetworkSystem;
-using VoiceChat.Networking;
-
 
 namespace VoiceChat.Demo.HLAPI
 {
     public class OMGNetManager : NetworkManager
     {
-        //PlayerVoiceChat this_instance;
-        public override void OnStartClient(NetworkClient client)
-        {
-            VoiceChatNetworkProxy.OnManagerStartClient(client);
-            base.OnStartClient(client);
-        }
-
-        public override void OnStopClient()
-        {
-            //base.OnStopClient();
-            VoiceChatNetworkProxy.OnManagerStopClient();
-
-            if (client != null)
-            {
-              Destroy(GetComponent<PlayerVoiceChat>());
-            }
-
-        }
-
-        public override void OnServerDisconnect(NetworkConnection conn)
-        {
-            base.OnServerDisconnect(conn);
-            VoiceChatNetworkProxy.OnManagerServerDisconnect(conn);
-        }
-
-		/* Called from NetworkManager.StartServer()
-		*  This is also the implementation for a Host
-		*/
-        public override void OnStartServer()
-        {
-		//	base.OnStartServer(); //Default implementation
-			VoiceChatNetworkProxy.OnManagerStartServer(); //Voice chat implementation
-        }
-
-        public override void OnStopServer()
-        {
-            //base.OnStopServer();
-            VoiceChatNetworkProxy.OnManagerStopServer();
-            //Destroy(GetComponent<VoiceChatServerUi>());
-        }
-
-        public override void OnClientConnect(NetworkConnection connection)
-        {
-            base.OnClientConnect(connection);
-            VoiceChatNetworkProxy.OnManagerClientConnect(connection);
-        }
+        
     }
 }
