ddd34ffea87504acaed1cbd80d43519c10c80bf1
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-06 02:20:07 +0000

    rearranged the UI, added character picker screen, created folder for CharacterKit, ClueKit

---
+++ Assets/Assets/_src/CharacterKit/Specs/CharacterSpec.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pirate", menuName = "MurderMystery/Character", order = 1)]
+public class CharacterSpec : ScriptableObject
+{
+    public string FullName = "Captain Hook";
+    public string NickName = "Hook";
+    public int age = 35;
+    public string Bio = "Attack!!!";
+    public GameObject CharacterPrefab = null;
+}
