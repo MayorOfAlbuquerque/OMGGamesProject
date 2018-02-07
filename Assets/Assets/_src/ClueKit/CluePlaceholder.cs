3f6636f5eae5a1c676ff131fbec54fc539b1c224
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-07 15:51:27 +0000

    clue preview working

--- Assets/Assets/_src/ClueKit/CluePlaceholder.cs
+++ Assets/Assets/_src/ClueKit/CluePlaceholder.cs
@@ -4,22 +4,65 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class CluePlaceholder : MonoBehaviour{
     public ClueSpec Clue;
-    public bool showClueModel = false;
+    public bool previewClue = false;
     private Color placeholderColour = new Color(1.0f, 0.0f, 0.0f, 0.75f);
+
+    private GameObject prefabInstance;
+
+    void Start()
+    {
+        InstantiateCluePrefab();
+    }
+    void InstantiateCluePrefab() {
+        RemovePrefabs();
+        if (Clue != null && Clue.ModelPrefab != null && prefabInstance == null)
+        {
+            prefabInstance = Instantiate(Clue.ModelPrefab);
+            prefabInstance.transform.position = transform.position;
+            prefabInstance.transform.parent = transform;
+            prefabInstance.SetActive(false);
+        }
+    }
+    void Update() {
+        if(previewClue && prefabInstance == null) {
+            InstantiateCluePrefab();
+            prefabInstance.SetActive(true);
+        } else if(previewClue) {
+            prefabInstance.SetActive(true);
+        } else {
+            prefabInstance.SetActive(false);
+        }
+    }
     void OnDrawGizmos() 
     {
-        if (showClueModel && Clue != null)
+        if (!previewClue)
         {
-            Mesh clueMesh = Clue
-                .ModelPrefab
-                .GetComponent<MeshFilter>()
-                .sharedMesh;
-            Gizmos.DrawMesh(clueMesh, transform.position, transform.rotation, transform.localScale);
-        }
-        else
-        {
-            Gizmos.color = placeholderColour;
             Gizmos.DrawIcon(transform.position, "question_mark.png", false);
         }
+    }
+
+    private void OnDisable()
+    {
+        RemovePrefabs();
+    }
+
+    void RemovePrefabs() {
+        foreach (Transform child in transform)
+        {
+            if (Application.isEditor)
+            {
+                DestroyImmediate(child.gameObject);
+
+            }
+            else
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RemovePrefabs();
     }
 }
---
+++ Assets/Assets/_src/ClueKit/ClueSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClueSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SpawnCluesForCurrentScene();
+	}
+
+    public void SpawnCluesForCurrentScene() {
+        Scene activeScene = SceneManager.GetActiveScene();
+        if (activeScene != null)
+        {
+            SpawnClues(activeScene);
+        }
+    }
+
+    public void SpawnClues(Scene scene) {
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+
+        CluePlaceholder[] placeholders;
+        foreach(GameObject obj in rootObjects) {
+            placeholders = obj.GetComponentsInChildren<CluePlaceholder>();
+            foreach (CluePlaceholder placeholder in placeholders)
+                SpawnClueInScene(placeholder);    
+        }
+    }
+
+    private void SpawnClueInScene(CluePlaceholder placeholder) {
+        if(placeholder != null && placeholder.Clue && placeholder.Clue.ModelPrefab != null)
+        {
+            if(!placeholder.gameObject.activeInHierarchy) {
+                return;
+            }
+            placeholder.gameObject.SetActive(false);
+            Instantiate(
+                placeholder.Clue.ModelPrefab,
+                placeholder.transform.position,
+                placeholder.transform.rotation
+            );    
+        }
+    }
+}
