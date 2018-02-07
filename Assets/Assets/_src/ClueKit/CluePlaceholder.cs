9cab9eed517fc8f5a9cacc128526be5ffb3db16c
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-07 14:59:18 +0000

    clue kit can render model, but has problem with prefabs with children

---
+++ Assets/Assets/_src/ClueKit/CluePlaceholder.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CluePlaceholder : MonoBehaviour{
+    public ClueSpec Clue;
+    public bool showClueModel = false;
+    private Color placeholderColour = new Color(1.0f, 0.0f, 0.0f, 0.75f);
+    void OnDrawGizmos() 
+    {
+        if (showClueModel && Clue != null)
+        {
+            Mesh clueMesh = Clue
+                .ModelPrefab
+                .GetComponent<MeshFilter>()
+                .sharedMesh;
+            Gizmos.DrawMesh(clueMesh, transform.position, transform.rotation, transform.localScale);
+        }
+        else
+        {
+            Gizmos.color = placeholderColour;
+            Gizmos.DrawIcon(transform.position, "question_mark.png", false);
+        }
+    }
+}
