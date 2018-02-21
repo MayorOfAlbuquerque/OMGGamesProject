6528e835972c3c32464ac00a3d59b886ad8b0a97
Sydney <sd14814@my.bristol.ac.uk>
2018-02-21 18:20:05 +0000

    Beacons definately work, transform issues.

--- Assets/Assets/_src/Scripts/BeaconMoveScript.cs
+++ Assets/Assets/_src/Scripts/BeaconMoveScript.cs
@@ -10,25 +10,35 @@ public class BeaconMoveScript : MonoBehaviour {
 	[SerializeField]
 	public GameObject Beacon2;
 	private int lastReceived = 1;
+	private Vector3 B1;
+	private Vector3 B2;
+	public GameObject cube;
+	[SerializeField]
+	public GameObject player;
 
 
 	// Use this for initialization
 	void Start () {
+		cube = GameObject.FindGameObjectWithTag ("Test");
 		// Acces the android java receiver we made
 		jc = new AndroidJavaClass("com.example.omg.myapplication.UnityReceiver");
-		Debug.LogError (jc);
+		//Debug.LogError (jc);
 		// We call our java class function to create our MyReceiver java object
 		jc.CallStatic("createInstance");
+		B1 = Beacon1.transform.position;
+		B2 = Beacon1.transform.position;
 		
 	}
 	// Update is called once per frame
 	void Update () {
 		int.TryParse(jc.GetStatic<string> ("intentMessage"), out javaMessage);
 		if ((javaMessage == 1) && (lastReceived != 1)) {
-			this.gameObject.transform.position = Beacon1.transform.position;
+			cube.SetActive (true);
+			gameObject.transform.position = new Vector3 (0, 0, 0);
 			lastReceived = 1;
 		} else if ((javaMessage == 2) && (lastReceived!=2)) {
-			this.gameObject.transform.position = Beacon2.transform.position;
+			cube.SetActive (false);
+			gameObject.transform.position = new Vector3 (100, 100, 100);
 			lastReceived = 2;
 		}
 
