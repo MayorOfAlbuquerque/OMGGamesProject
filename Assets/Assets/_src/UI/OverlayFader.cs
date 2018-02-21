ad7e72e27cd35c18ac1d3fb84460c4a830fa300b
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-21 23:48:57 +0000

    adding character specs for

---
+++ Assets/Assets/_src/UI/OverlayFader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OverlayFader : MonoBehaviour {
+    public float targetAlpha = 0.0f;
+    public float startAlpha = 1.0f;
+    private float startTime = 0.0f;
+    public float duration = 0.5f;
+    public Image image;
+	// Use this for initialization
+	void Start () {
+        image.color = new Color(0, 0, 0, startAlpha);
+	}
+	
+    public void FadeToClear() {
+        startTime = Time.time;
+        startAlpha = 1.0f;
+        targetAlpha = 0.0f;
+    }
+
+    public void FadeToBlack() {
+        startTime = Time.time;
+        startAlpha = 0.0f;
+        targetAlpha = 1.0f;
+    }
+	// Update is called once per frame
+	void Update () {
+        float t = (Time.time - startTime) / duration;
+        float alpha = Mathf.SmoothStep(startAlpha, targetAlpha, t);
+        image.color = new Color(0, 0, 0, alpha);
+	}
+}
