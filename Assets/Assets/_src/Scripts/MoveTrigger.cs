c14eb953e3be494e427238ab2dbdc36b079bd944
Rachel Jeffries-Harris <racheljeffriesharris@gmail.com>
2018-02-22 15:03:29 +0000

    Jammy but now moves to the second carraige when enters the collider

--- Assets/Assets/_src/Scripts/MoveTrigger.cs
+++ Assets/Assets/_src/Scripts/MoveTrigger.cs
@@ -6,13 +6,14 @@ public class MoveTrigger : MonoBehaviour {
 	[SerializeField] GameObject Beacon2;
 	
 		void OnTriggerEnter(Collider other){
+			Vector3 moveVector = Beacon2.transform.position - other.gameObject.transform.position;
+			other.gameObject.GetComponent<CharacterController> ().Move (moveVector);
 			Debug.Log ("Object is within the collider");
 		}
 
 		void OnTriggerStay(Collider other){
-			other.gameObject.transform.position = Beacon2.transform.position;
+//			other.gameObject.transform.position = Beacon2.transform.position;
 			Debug.Log ("Object is within the trigger");
-			
 		}
 
 		void OnTriggerExit(Collider other){
