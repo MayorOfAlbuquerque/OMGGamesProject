2e161d2d4cc3d6ac47bba1f90d3d44f461836c85
Zaiyang Li <zaiyangli777@gmail.com>
2018-02-24 15:31:10 +0000

    player join game and custom models

---
+++ Assets/Assets/_src/Scripts/Networking/PlayerClientManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerClientManager : MonoBehaviour {
+
+    public bool joinOnSceneLoad;
+    public GameSettings settings;
+    private NetworkClient client;
+    private bool joined = false;
+
+    public void Start()
+    {
+        client = NetworkManager.singleton?.client;
+        if(joinOnSceneLoad) {
+            //JoinServer();
+        }
+    }
+
+    public void Init(NetworkClient client)
+    {
+        this.client = client;
+
+    }
+
+    void Update()
+    {
+        JoinServer();    
+    }
+    public void JoinServer()
+    {
+        if (joined || client == null)
+        {
+            return;
+        }
+        PlayerJoinMessage message = new PlayerJoinMessage(
+            client.connection.connectionId,
+            settings.CharacterId
+        );
+        ClientScene.AddPlayer(client.connection, 0, message);
+        //client.Send(PlayerJoinMessage.MESSAGE_TYPE, message);
+        joined = true;
+    }
+}
