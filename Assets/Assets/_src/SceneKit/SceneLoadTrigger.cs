765715b7f7edc78d952b16e1a6e75b4f5742d02a
Zaiyang Li <zaiyangli777@gmail.com>
2018-03-12 15:29:52 +0000

    scene trigger

--- Assets/Assets/_src/SceneKit/SceneLoadTrigger.cs
+++ Assets/Assets/_src/SceneKit/SceneLoadTrigger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Networking;
 public class SceneLoadTrigger : MonoBehaviour {
     private bool loaded;
 
